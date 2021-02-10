c8e496a096edc9f3e9b94d0728da168648249104
incia94 <anandincia@gmail.com>
2021-02-11 01:52:47 +0530

    comments

---
+++ practice_builtin_modules/CalenderModule.py
@@ -0,0 +1,6 @@
+import calendar
+
+
+print("The calender of this year is:")
+print(calendar.calendar(2021))
+
---
+++ practice_builtin_modules/OsModule.py
@@ -0,0 +1,7 @@
+import os
+
+print(os.name)      # Name of the Operating system 'posix', 'nt', 'os2', 'ce', 'java' and 'riscos'
+print('\n')
+print(os.getcwd())  # get working directory
+print('\n')
+print(os.)
